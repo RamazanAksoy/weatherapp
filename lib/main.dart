2e203e7bc7b1508a34c3b7d63817877f80a4c28c
RamazanAksoy <ramazan.aksoy1907@hotmail.com>
2022-11-15 14:45:28 +0300

    mvc entegration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:weatherapp/providers/weather_provider.dart';
-import 'home.dart';
+import 'package:weatherapp/ui/home/model-view/weather_provider.dart';
+import 'ui/home/view/home.dart';
 
 void main() {
   runApp(MultiProvider(
@@ -18,7 +18,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return ResponsiveSizer(builder: (context, orientation, screenType) {
@@ -30,7 +29,6 @@ class MyApp extends StatelessWidget {
       );
     });
   }
-
 }
 
 class MyHomePage extends StatefulWidget {
